06af6c339eb2fcbf9d8b7fca76b2005fc7fd4879
wsy <2553241022@qq.com>
2021-12-22 17:30:01 +0800

    :white_check_mark: test(packages): 这是一次测试git
    啦啦啦

--- .cz-config.js
+++ .cz-config.js
@@ -2,14 +2,14 @@
  * @Description:
  * @Author: wsy
  * @Date: 2021-12-17 15:23:59
- * @LastEditTime: 2021-12-20 19:48:43
+ * @LastEditTime: 2021-12-22 17:28:48
  * @LastEditors: wsy
  */
 module.exports = {
-  // 可选类型
   types: [
     { value: ':sparkles: feat', name: '✨ feat:     新功能' },
     { value: ':bug: fix', name: '🐛 fix:      修复' },
+    { value: ':tada: init', name: '🎉 Init:      初始化' },
     { value: ':memo: docs', name: '📝 docs:     文档变更' },
     { value: ':lipstick: style', name: '💄 style:    代码格式(不影响代码运行的变动)' },
     {
@@ -19,7 +19,9 @@ module.exports = {
     { value: ':zap: perf', name: '⚡️ perf:     性能优化' },
     { value: ':white_check_mark: test', name: '✅ test:     增加测试' },
     { value: ':wrench: chore', name: '🔧 chore:    构建过程或辅助工具的变动' },
-    { value: ':rewind: revert', name: '⏪️ revert:   回退' }
+    { value: ':package: build', name: '📦‍ build:    打包构建' },
+    { value: ':rewind: revert', name: '⏪️ Revert:   回退' },
+    { value: ':construction_worker: ci', name: '👷 ci:   CI 配置' }
   ],
   // 消息步骤
   messages: {
--- commitlint.config.js
+++ commitlint.config.js
@@ -2,7 +2,7 @@
  * @Description:
  * @Author: wsy
  * @Date: 2021-12-17 16:03:11
- * @LastEditTime: 2021-12-20 19:47:59
+ * @LastEditTime: 2021-12-22 17:20:44
  * @LastEditors: wsy
  */
 module.exports = {
@@ -10,22 +10,6 @@ module.exports = {
   extends: ['gitmoji'],
   // 定义规则类型
   rules: {
-    // type 类型定义，表示 git 提交的 type 必须在以下类型范围内
-    'type-enum': [
-      2,
-      'always',
-      [
-        ':sparkles:', // 新功能 feature
-        ':bug:', // 修复 bug
-        ':memo:', // 文档注释
-        ':lipstick:', // 代码格式(不影响代码运行的变动)
-        ':recycle:', // 重构(既不增加新功能，也不是修复bug)
-        ':zap:', // 性能优化
-        ':white_check_mark:', // 增加测试
-        ':wrench:', // 构建过程或辅助工具的变动
-        ':rewind:' // 回退
-      ]
-    ],
     // subject 大小写不做校验
     'subject-case': [0]
   }
