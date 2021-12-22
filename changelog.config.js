5821d0813491258527caeae57eb875bc9fe1f563
wsy <2553241022@qq.com>
2021-12-22 18:41:49 +0800

    :bug: fix(ss): 修改

--- changelog.config.js
+++ changelog.config.js
@@ -1,13 +1,20 @@
+/*
+ * @Description:
+ * @Author: wsy
+ * @Date: 2021-12-22 18:36:33
+ * @LastEditTime: 2021-12-22 18:41:19
+ * @LastEditors: wsy
+ */
 module.exports = {
-  withEmoji: true;
+  withEmoji: true,
   /**
    * title language
    * @default en-US
    */
-  titleLanguage: 'zh-CN';
+  titleLanguage: 'zh-CN',
   /**
    * whether to show author
    * @default false
    */
-  showAuthor: true;
-};
+  showAuthor: true
+}
