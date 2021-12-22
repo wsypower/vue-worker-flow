6a72bca1665cc38b13a94696e2c40f04b2c06d4b
wsy <2553241022@qq.com>
2021-12-22 19:00:07 +0800

    :memo: docs(123): 这是一次变更
    碧昂恒拉

--- commitlint.config.js
+++ commitlint.config.js
@@ -2,7 +2,7 @@
  * @Description:
  * @Author: wsy
  * @Date: 2021-12-17 16:03:11
- * @LastEditTime: 2021-12-22 17:20:44
+ * @LastEditTime: 2021-12-22 18:59:47
  * @LastEditors: wsy
  */
 module.exports = {
@@ -10,7 +10,7 @@ module.exports = {
   extends: ['gitmoji'],
   // 定义规则类型
   rules: {
-    // subject 大小写不做校验
+    // subject 大小写不做校 验
     'subject-case': [0]
   }
 }
