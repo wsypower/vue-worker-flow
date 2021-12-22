6f9584671617f96da443d899511630e34bf6db6b
wsy <2553241022@qq.com>
2021-12-22 18:37:07 +0800

    :bug: fix(123): 这是标题

--- .changelogrc.js
+++ .changelogrc.js
@@ -1,22 +1,10 @@
-/*
- * @Description:
- * @Author: wsy
- * @Date: 2021-12-22 18:29:18
- * @LastEditTime: 2021-12-22 18:31:04
- * @LastEditors: wsy
- */
-
 module.exports = {
-  /**
-   * whether to include emoji in title
-   * @default true
-   */
   withEmoji: true;
   /**
    * title language
    * @default en-US
    */
-  titleLanguage: 'en-US' | 'zh-CN';
+  titleLanguage: 'zh-CN';
   /**
    * whether to show author
    * @default false
