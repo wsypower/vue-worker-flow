43477f4d9f44a136f5c5031626b622699485b91c
wsy <2553241022@qq.com>
2021-12-22 19:58:32 +0800

    :sparkles: feat(packages): 梳理

--- commitlint.config.js
+++ commitlint.config.js
@@ -2,15 +2,12 @@
  * @Description:
  * @Author: wsy
  * @Date: 2021-12-17 16:03:11
- * @LastEditTime: 2021-12-22 18:59:47
+ * @LastEditTime: 2021-12-22 19:15:35
  * @LastEditors: wsy
  */
 module.exports = {
-  // 继承的规则
   extends: ['gitmoji'],
-  // 定义规则类型
   rules: {
-    // subject 大小写不做校 验
     'subject-case': [0]
   }
 }
